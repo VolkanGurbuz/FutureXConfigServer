0ea85100f68f6fd3fff7f000830ade68000f1370
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-06 18:23:55 +0200

    Initial commit

---
+++ src/main/java/com/volkangurbuz/FutureXConfigServer/FutureXConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.volkangurbuz.FutureXConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class FutureXConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureXConfigServerApplication.class, args);
+	}
+
+}
